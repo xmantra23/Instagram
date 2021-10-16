d9496c4aa91ef55835bef1b57c60d89325ce3f59
xmantra23 <samir.shrestha92@gmail.com>
2021-10-16 12:46:20 -0500

    implemented part2

--- app/src/main/java/com/samir/instagram/Post.java
+++ app/src/main/java/com/samir/instagram/Post.java
@@ -1,16 +1,18 @@
 package com.samir.instagram;
 
-import com.parse.Parse;
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+
+import java.util.Date;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_KEY = "createdAt";
 
     public String getDescription(){
         return getString(KEY_DESCRIPTION);
---
+++ app/src/main/java/com/samir/instagram/fragments/PostsFragment.java
@@ -0,0 +1,100 @@
+package com.samir.instagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.samir.instagram.Post;
+import com.samir.instagram.PostsAdapter;
+import com.samir.instagram.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PostsFragment extends Fragment {
+    public static final String TAG = "PostFragment";
+    private RecyclerView rvPosts;
+    protected PostsAdapter adapter;
+    protected SwipeRefreshLayout swipeContainer;
+    protected List<Post> allPosts;
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(),allPosts);
+        swipeContainer = view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                queryPosts();
+            }
+        });
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+    }
+
+    public void clear() {
+        allPosts.clear();
+        adapter.notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> posts) {
+        allPosts.addAll(posts);
+        adapter.notifyDataSetChanged();
+    }
+
+
+
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG,"Issue with getting posts",e);
+                    return;
+                }
+                for(Post post: posts){
+                    Log.i(TAG,"Post: " + post.getDescription() + ",username " + post.getUser().getUsername());
+                }
+                allPosts.clear();
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
